eeffac4c3c9148f3a6100728826ce375af2eff97
Sachin Karia <sachin.karia01@gmail.com>
2016-05-24 14:19:54 +0100

    working api mvp

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,12 @@ module ComeDineAPI
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.api_only = true
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end

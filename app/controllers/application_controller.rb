442ec071fb6dbb7c522a3212949623b3b9b780ed
Sachin Karia <sachin.karia01@gmail.com>
2016-05-31 10:31:24 +0100

    changed header refresh config to false

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+
   protect_from_forgery with: :null_session
   respond_to :json
+
+
 end
